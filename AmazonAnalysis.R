aa57b75b349b5a1dcb226647214eb583924f83c4
Justin Ross <justinross102@gmail.com>
2023-10-09 14:42:58 -0600

    add penalized logistic regression to analysis

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -82,6 +82,53 @@ vroom_write(x=kaggle_submission, file="./amazon_predictions.csv", delim=",")
 # private - 0.70429
 # public - 0.69688
 
+# penalized logistic regression -------------------------------------------
+
+penalized_logistic_mod <- logistic_reg(mixture = tune(),
+                                       penalty = tune()) %>% #Type of model
+  set_engine("glmnet")
+
+penalized_logistic_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(penalized_logistic_mod)
+
+## Grid of values to tune over
+pen_tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 5) ## L^2 total tuning possibilities
+
+## Split data for CV15
+pen_folds <- vfold_cv(train, v = 5, repeats = 1)
+
+## Run the CV
+CV_results <- penalized_logistic_workflow %>%
+  tune_grid(resamples = pen_folds,
+            grid = pen_tuning_grid,
+            metrics = metric_set(roc_auc)) # f_meas, sens, recall, spec, precision, accuracy
+
+## Find Best Tuning Parameters
+pen_bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+## Finalize the Workflow & fit it
+final_pen_wf <- penalized_logistic_workflow %>%
+  finalize_workflow(pen_bestTune) %>%
+  fit(data = train)
+
+## Predict
+penalized_logistic_predictions <- final_pen_wf %>%
+  predict(new_data = test,
+          type = "prob")
+
+pen_log_submission <- penalized_logistic_predictions %>%
+  mutate(Id = row_number()) %>% 
+  rename("Action" = ".pred_1") %>% 
+  select(3,2)
+
+# write to csv
+vroom_write(x=pen_log_submission, file="./penalized_logistic_predictions.csv", delim=",")
+# private - 0.70442
+# public - 0.69667
 
 
 
