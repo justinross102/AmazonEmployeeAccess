5bdfc78a8edaf1c701c612fd31b0516ecd268aec
Justin Ross <justinross102@gmail.com>
2023-10-09 13:58:28 -0600

    add logistic regression to analysis

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -10,8 +10,11 @@ library(embed) # for target encoding
 library(ggmosaic)
 
 # load in data ------------------------------------------------------------
-train <- vroom("./train.csv")
-test <- vroom("./test.csv")
+train <- vroom("./train.csv") %>% 
+  mutate(ACTION = as.factor(ACTION))
+
+test <- vroom("./test.csv") %>% 
+  select(-1)
 
 # plots -------------------------------------------------------------------
 
@@ -48,9 +51,38 @@ ggplot(subset_df, aes(x = factor(MGR_ID), fill = factor(ACTION))) +
 my_recipe <- recipe(ACTION ~ ., data = train) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
   step_other(all_nominal_predictors(), threshold = .01) %>%  # combines categorical values that occur <1% into an "other" value
-  step_dummy(all_nominal_predictors()) # dummy variable encoding
+  step_dummy(all_nominal_predictors())  # dummy variable encoding
   
 # apply the recipe to the data
 prepped_recipe <- prep(my_recipe)
 baked <- bake(prepped_recipe, new_data = train) # should have 112 columns
 baked
+
+# logistic regression ------------------------------------------------------
+
+logistic_mod <- logistic_reg() %>% 
+  set_engine("glm")
+
+logistic_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(logistic_mod) %>%
+  fit(data = train) # Fit the workflow
+
+amazon_predictions <- predict(logistic_workflow,
+                              new_data = test,
+                              type = "prob") # "class" or "prob"
+
+kaggle_submission <- amazon_predictions %>%
+  mutate(Id = row_number()) %>% 
+  rename("Action" = ".pred_1") %>% 
+  select(3,2)
+
+# write predictions to csv
+vroom_write(x=kaggle_submission, file="./amazon_predictions.csv", delim=",")
+# private - 0.70429
+# public - 0.69688
+
+
+
+
+
